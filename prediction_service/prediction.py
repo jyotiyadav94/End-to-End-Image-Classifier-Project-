a0f134465fa67390d6119837a80c5ca78b69b73a
Jyoti Yadav <72126242+jyotiyadav94@users.noreply.github.com>
2024-06-29 12:00:15 +0200

    Update prediction.py

--- prediction_service/prediction.py
+++ prediction_service/prediction.py
@@ -10,6 +10,7 @@ from timm.data.transforms_factory import create_transform
 
 params_path = "params.yaml"
 
+
 class InvalidImage(Exception):
     def __init__(self, message="Invalid image format or content"):
         self.message = message
